74ac2a744a4d18d2ab0ca5b3877e2814aa5b60ec
Emmanuel Telmon <etelmon@zimit.io>
2016-09-14 15:30:09 -0400

    issue with jasmine

--- spec/server-sync.spec.js
+++ spec/server-sync.spec.js
@@ -2,12 +2,18 @@ var request = require("request");
 var sync = require("../lib/server-sync");
 var Promise = require('promise');
 var base_url = "http://localhost:3000/";
-
+var socket;
 var handler;
 var userId;
+var deferred;
 
 describe("Sync", function () {
     beforeEach(function () {
+        deferred = {};
+        deferred.promise = new Promise(function (resolve, reject) {
+            deferred.resolve = resolve;
+        });
+
         userId = 'UID1234';
         sync.publish(
             'magazines',
@@ -15,40 +21,57 @@ describe("Sync", function () {
                 return Promise.resolve([]);
             },
             "MAGAZINE");
+
+        socket = {
+            on: function (event, callback) {
+                console.log('Socket.on:' + event);
+            },
+            emit: function (event, params, callback) {
+                console.log('Socket.emit:' + event + '->' + JSON.stringify(params));
+                deferred.resolve(params);
+            }
+        };
+
         handler = {
             user: {
                 tenantId: 'TID',
                 id: userId,
-                display:'John'
+                display: 'John'
             },
-            socket: {
-                on: function (event, callback) {
-                    console.log('Socket.on:' + event);
-                },
-                emit: function (event, params, callback) {
-                    console.log('Socket.emit:' + event+'->'+JSON.stringify(params));
-                }
-            }
-        }
+            socket: socket
+        };
+
+        spyOn(socket, 'emit').and.callThrough();
 
     });
+
+    afterEach(function () {
+        sync.clear();
+    });
+
     it("should subscribe and receive the subscription id", function () {
-        //expect(1).toEqual(2);
         var subId = sync.subscribe(handler, null, 'magazines', null);
         expect(sync.countActiveSubscriptions()).toBe(1);
         expect(subId).toBeDefined();
 
     });
 
+    it("should subscribe and receive subscription id", function (done) {
+
+        var subId = sync.subscribe(handler, null, 'magazines', null);
+        waitForNotification().then(function (sub) {
+            done();
+        })
+
+    });
+
     it("should unsubscribe", function () {
-        //expect(1).toEqual(2);
         var subId = sync.subscribe(handler, null, 'magazines', null);
         expect(subId).toBeDefined();
         sync.unsubscribe(userId, subId);
         expect(sync.countActiveSubscriptions()).toBe(0);
-
-        // because sync still has the previous connection!!! need to tear down or reinit sync..
     });
+
 
     // it("returns status code 200", function (done) {
     //     done();
@@ -68,4 +91,9 @@ describe("Sync", function () {
     //         });
     //     });
     // });
+
+    function waitForNotification() {
+        return deferred.promise;
+    }
+
 });
